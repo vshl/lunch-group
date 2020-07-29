9102bd76135623700a4cf7f01e33cc461a3349c9
Vishal Ravi Shankar <rsh.vishal@gmail.com>
2020-07-29 13:04:31 +0530

    Unit test for the controller method to create groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :groups
   root 'employees#index'
+  resources :groups
   resources :events
   resources :employees
+  post 'create_groups' => 'groups#create_groups', as: :create_groups
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
